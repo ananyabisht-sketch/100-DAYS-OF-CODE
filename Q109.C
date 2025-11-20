f361036b88ba68c9fca9d335713c88f6cc12eabb
ananyabisht-sketch <ab.ananyabisht@gmail.com>
2025-11-20 19:42:41 +0530

    Create Q109.C

---
+++ Q109.C
@@ -0,0 +1,32 @@
+Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k.
+
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (k > n) {
+        printf("0\n");
+        return 0;
+    }
+ int windowSum = 0;
+    for (int i = 0; i < k; i++)
+        windowSum += arr[i];
+
+    int maxSum = windowSum;
+
+   
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];
+        if (windowSum > maxSum)
+            maxSum = windowSum;
+    }
+
+    printf("%d\n", maxSum);
+
+    return 0;
+}
