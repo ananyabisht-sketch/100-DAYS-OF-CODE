e4b8f1c7a4fb5461bb46f7d4b8d68c08df44a2c6
ananyabisht-sketch <ab.ananyabisht@gmail.com>
2025-11-22 19:28:55 +0530

    Create Q132.C

---
+++ Q132.C
@@ -0,0 +1,33 @@
+//Define an enum for traffic lights (RED, YELLOW, GREEN) and print 'Stop', 'Wait', or 'Go' based on its value.
+
+#include <stdio.h>
+enum Days {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+};
+
+int main() {
+    enum Days day;
+
+    printf("Day\t\tValue\n");
+    printf("---------------------\n");
+
+    for (day = SUNDAY; day <= SATURDAY; day++) {
+        switch (day) {
+            case SUNDAY:    printf("SUNDAY\t\t%d\n", day); break;
+            case MONDAY:    printf("MONDAY\t\t%d\n", day); break;
+            case TUESDAY:   printf("TUESDAY\t\t%d\n", day); break;
+            case WEDNESDAY: printf("WEDNESDAY\t%d\n", day); break;
+            case THURSDAY:  printf("THURSDAY\t%d\n", day); break;
+            case FRIDAY:    printf("FRIDAY\t\t%d\n", day); break;
+            case SATURDAY:  printf("SATURDAY\t%d\n", day); break;
+        }
+    }
+
+    return 0;
+}
